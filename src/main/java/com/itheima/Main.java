d675773bdaf57ffab55b2d9550ae09b29df807d6
admin <111@163.com>
2019-04-28 12:18:52 +0800

    senc commit

--- src/main/java/com/itheima/Main.java
+++ src/main/java/com/itheima/Main.java
@@ -11,5 +11,6 @@ package com.itheima;
 public class Main {
     public static void main(String[] args) {
         System.out.println("ooooo");
+        System.out.println("ooooo11231131");
     }
 }
