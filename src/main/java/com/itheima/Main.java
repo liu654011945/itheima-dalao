0d069392807de039684512103ff0f13f65e0da39
admin <111@163.com>
2019-04-28 12:15:30 +0800

    first commit

---
+++ src/main/java/com/itheima/Main.java
@@ -0,0 +1,15 @@
+package com.itheima;
+
+/**
+ * 描述
+ *
+ * @author 三国的包子
+ * @version 1.0
+ * @package com.itheima *
+ * @since 1.0
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("ooooo");
+    }
+}
